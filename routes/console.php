4891b6a9afe0fcaaeb3a8c7652a38c80a1be1386
Sandy <sandytriantono816@gmail.com>
2025-12-08 09:28:02 +0700

    reset password guru

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('app:reset-guru-passwords', function () {
+    $newPassword = 'password';
+    $hashed = Hash::make($newPassword);
+
+    $count = User::where('role', User::ROLE_GURU)->update(['password' => $hashed]);
+
+    $this->info("Updated {$count} guru passwords to '{$newPassword}'.");
+})->purpose('Force reset all guru passwords to a fixed value');
